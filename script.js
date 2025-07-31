934766d8f1304a32c27b02ab832fc796526bb3ba
Sachin-ll <sachin.pund@liquiloans.com>
2025-07-31 23:12:23 +0530

    new

---
+++ script.js
@@ -0,0 +1,7 @@
+function addNumbers() {
+    const number1 = parseFloat(document.getElementById('num1').value);
+    const number2 = parseFloat(document.getElementById('num2').value);
+    const sum = number1 + number2;
+  
+    document.getElementById('result').textContent = sum;
+  }
